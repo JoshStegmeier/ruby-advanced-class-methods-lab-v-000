293445ad978af8ac4ec0c0ed0bc23e63fb7ce492
Josh Stegmeier <josh@steg.me>
2018-06-03 19:47:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,7 @@ class Song
 
   def self.find_or_create_by_name(song_name)
     search = self.find_by_name(song_name)
-    search_name = self.find_by_name(song_name).name
-    if search_name == song_name
+    if search == song_name
       search
     else
       self.create_by_name(song_name)
