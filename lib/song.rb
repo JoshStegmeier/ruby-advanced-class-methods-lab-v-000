4167433bb38860da4b03b7c4ec88b17101b1a976
Josh Stegmeier <josh@steg.me>
2018-06-03 19:35:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,10 @@ class Song
     song
   end
 
-  def self.new_by_name(name)
-    newInstance = self.create
-    @name = name
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = name
+    song
   end
 
 end
